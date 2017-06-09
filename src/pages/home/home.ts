57c8d7ad9847db9e496d1bae82dad58bbc1afb06
berk <berkakkerman@hotmail.com>
2017-06-09 21:53:11 +0300

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,66 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {Modal, NavController, Alert} from 'ionic-angular';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
+
+//import {YoutubeService} from '../../providers/youtube-service/youtube-service';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers:[]
 })
 export class HomePage {
+  data:any;
+  title:any;
+  channelID: string = 'UCtb8P4rf_1n8KS8eZk_lNNw';
+  maxResults: string = '10';
+  pageToken: string; 
+  googleToken: string = 'Your api key';
+  searchQuery: string = '';
+  posts: any = [];
+  onPlaying: boolean = false; 
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public http: Http, public nav:NavController, private youtube: YoutubeVideoPlayer) {
+    this.loadSettings();
   }
+/*
+  launchYTPlayer(id, title): void {
+    this.ytPlayer.launchPlayer(id, title);
+  }
+*/
+  fetchData() {
+    return new Promise(resolve=>{
+       let url = 'https://www.googleapis.com/youtube/v3/search?part=id,snippet&channelId=' + this.channelID + '&q=' + this.searchQuery + '&type=video&order=viewCount&maxResults=' + this.maxResults + '&key=' + this.googleToken;
 
+    if(this.pageToken) {
+      url += '&pageToken=' + this.pageToken;
+    }
+    console.log(url);
+      this.http.get(url).map(res => res.json()).subscribe(data => { 
+        this.title= data.items[0].snippet.channelTitle;
+        this.data=data.items;
+        console.log(data);
+        
+        console.log(this.data);
+        
+        resolve(this.data); 
+      });
+    });
+  }
+  loadSettings(): void {
+      this.fetchData();
+  }
+  openSettings(): void {
+      console.log("TODO: Implement openSettings()");
+  }
+  playVideo(e, post): void {
+      console.log(post);
+      this.onPlaying = true;
+      this.youtube.openVideo(post.id.videoId);
+      //this.ytPlayer.launchPlayer(post.id, post.snippet.title);
+  }
+  loadMore(): void {
+      console.log("TODO: Implement loadMore()");
+  }
 }
